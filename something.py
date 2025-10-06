f5c1163fe143ce349382ffbcc22bccc04d91f320
CynicalMrJones <wopjr1234@gmail.com>
2025-10-06 17:08:13 -0400

    More work

--- something.py
+++ something.py
@@ -24,6 +24,7 @@ class Token(Enum):
 
 
 charClass = int()
+# array of chars
 lexeme = []
 nextChar = ""
 lexLen = int()
@@ -46,6 +47,8 @@ def main():
 
 
 def getChar(file):
+    global nextChar
+    global charClass
     nextChar = file.read(1)
     if not nextChar:
         return 0
@@ -58,6 +61,7 @@ def getChar(file):
 
 
 def isspace(file):
+    global nextChar
     if nextChar == ' ':
         return True
     else:
@@ -65,6 +69,7 @@ def isspace(file):
 
 
 def getNonBlank(file):
+    global nextChar
     while isspace(nextChar):
         getChar(file)
 
@@ -72,6 +77,7 @@ def getNonBlank(file):
 def addChar():
     global lexLen
     global lexeme
+    global nextChar
     if lexLen <= 98:
         lexeme[lexLen + 1] = nextChar
         lexeme[lexLen] = 0
@@ -79,15 +85,65 @@ def addChar():
         print("Error lexeme is too long")
 
 
+def isPrint():
+    global lexeme
+    if (lexeme[0] == 'P' and
+        lexeme[1] == 'R' and
+        lexeme[2] == 'I' and
+        lexeme[3] == 'N' and
+        lexeme[4] == 'T' and
+        lexeme[5] == '\0'):
+        return True
+    else:
+        return False
+
+
+def lookup(char):
+    global nextToken
+    match char:
+        case '(':
+            addChar()
+            nextToken = Token.LEFT_PAREN.value
+        case ')':
+            addChar()
+            nextToken = Token.RIGHT_PAREN.value
+        case '+':
+            addChar()
+            nextToken = Token.ADD_OP.value
+        case '-':
+            addChar()
+            nextToken = Token.SUB_OP.value
+        case '*':
+            addChar()
+            nextToken = Token.MULT_OP.value
+        case '/':
+            addChar()
+            nextToken = Token.DIV_OP.value
+        case '=':
+            addChar()
+            nextToken = Token.ASSIGN_OP.value
+        case ';':
+            addChar()
+            nextToken = Token.SEMI_COLON.value
+        case _:
+            addChar()
+            nextToken = -1
+    return nextToken
+
+
 def lex(file):
     global lexLen
     global nextToken
+    global charClass
+    global strStmt
     lexLen = 0
+    getNonBlank(file)
     match charClass:
         case Characters.LETTER.value:
             addChar()
             getChar(file)
-            while charClass == Characters.LETTER.value or charClass == Characters.DIGIT.value:
+            while (charClass == Characters.LETTER.value or
+                   charClass == Characters.DIGIT.value):
                 addChar()
                 getChar(file)
             if isPrint():
@@ -110,6 +166,22 @@ def lex(file):
             lexeme[1] = 'O'
             lexeme[2] = 'F'
             lexeme[3] = 0
+    strStmt += lexeme
+    strStmt += " "
+    if nextToken == Token.SEMI_COLON.value:
+        print(f"{strStmt}")
+        strStmt = ""
+    return nextToken
+
+
+def updateVar(var, value):
+    global varMap
+    varMap[var] = value
+
+
+def getVarValue(var, val):
+    global varMap
+     it = varMap.get(var)
 
 
 if __name__ == "__main__":
